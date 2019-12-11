d045c91d6f1756a276e47890fef5cb490c28d06f
Thorvald van Beek <tvanbeek@roc-dev.com>
2019-12-11 10:29:16 +0100

    Willekeurig.php toegevoegd

---
+++ willekeurig.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+
+<link rel="stylesheet" href="style.css">
+<link href="icon.png" rel="icon" type="icon.png" />
+
+<head>
+    <title>Galgje | Willekeurig</title>
+</head>
+
+<body>
