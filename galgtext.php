738644c1fb9e6c5d898d29658a176dbdbbccb349
Thorvald van Beek <tvanbeek@roc-dev.com>
2019-12-13 10:09:21 +0100

    Cookie werkend gemaakt

--- galgtext.php
+++ galgtext.php
@@ -1,3 +1,7 @@
+<?php
+#session_start();
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -16,12 +20,32 @@
         <h3>Je hebt gekozen om zelf een woord in te voeren.</h3>
     </div>
     <div class="button">
-        <input type="text">
-        <button type="button">Start</button>
+        <form method="POST">
+            <input type="text" name="WoordGalgje" id="WoordGalgje">
+            <button type="submit" name="Opslaan" id="Start">Start</button>
+        </form>
+    </div>
+    <div class="phpGalgWoord">
+        <h1>
+            <?php
+            if (isset($_POST["Opslaan"])) {
+                    setcookie("Galgwoord", $_POST["WoordGalgje"]);
+                    header("refresh: 0 ");
+                }
+            
+            if (isset($_COOKIE["Galgwoord"])) {
+                echo ("Je hebt") . " " . $_COOKIE["Galgwoord"] . " " . "gekozen om mee te spelen!";
+            }
+            ?>
+        </h1>
     </div>
     <div class="img">
         <img src="END.png" alt="Galgje">
     </div>
+    <div class="startbutton">
+        <form>
+            <button class="startbutton" type="submit" formaction="">Begin het spel</button>
+        </form>
 </body>
 
 </html>
