7ba20a04506a0be3fcb4edfadb26e7cbd4ec4680
FIRST_TOM LAST_PAULUS <54597104+TPaulus24@users.noreply.github.com>
2019-12-19 19:52:07 +0100

    willekeurig.php werkende

--- willekeurig.php
+++ willekeurig.php
@@ -12,9 +12,28 @@
     <div class="head">
         <h1>Galgje</h1>
     </div>
+    
     <div class="text">
         <h3>Je hebt gekozen voor een willekeurig woord!</h3>
     </div>
+    <?php
+    $randomwoorden = array(
+    "academy",
+    "system",
+    "grip",
+    "shake",
+    "attack",
+    "number",
+    "angle",
+    "orange",
+    "tacky",
+    "rely",
+    "jar",
+    "touch"
+    );
+    $kiezen = array_rand($randomwoorden);
+    setcookie('woord' , $randomwoorden [$kiezen]);
+    ?>
     <div class="button">
         <form>
             <button class="startbutton" type="submit" formaction="galgspel.php">Start Willekeurig</button>
